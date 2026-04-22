af6d39d32afed38d3b00ac9df02d6043f425e021
jo261181 <jlavanderburgt@gmail.com>
2026-04-22 15:21:24 +0200

    remove unused form inputs from SimpleModal in Navigation component

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,6 @@
-import { Flex, Link, Button, Heading, Input } from "@chakra-ui/react";
+import { Flex, Link, Button, Heading } from "@chakra-ui/react";
 import SimpleModal from "../components/ui/modal";
 import { useState } from 'react';
-import { Form } from "react-router";
 
 export const Navigation = () => {
   const [modalOpen, setModalOpen] = useState(false);
@@ -16,17 +15,6 @@ export const Navigation = () => {
           title="New Event"
         >
           <Heading>Hello World!</Heading>
-          <Form
-            method="post"
-            action="/events/new"
-          >
-            <Input name="title" placeholder="Event Title" />
-            <Input name="description" placeholder="Event Description" />
-            <Input name="date" placeholder="Event date" />
-            <Input name="starttime" placeholder="Event start time" />
-            <Input name="endtime" placeholder="Event end time" />
-            <Button type="submit">Submit</Button>
-          </Form>
         </SimpleModal>
         <Button onClick={() => setModalOpen(true)}>Create Event</Button>
       </Flex>
